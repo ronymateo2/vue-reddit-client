64e602f5f075873e88f7f0ba7f0af399a0706363
ronymateo@gmail.com <ronymateo@gmail.com>
2019-11-29 09:54:36 -0500

    feat(app): add vuetify, vuex, vue-route

---
+++ src/views/Gallery.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an Gallery Page</h1>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    This is the post page
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {}
+};
+</script>
