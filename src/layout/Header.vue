012b12053725d3c9097dcf335a12a0ecd97d5dc3
ronymateo@gmail.com <ronymateo@gmail.com>
2019-11-29 10:08:58 -0500

    feat(layout): create layout with header to support post and gallery

---
+++ src/layout/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-toolbar
+    dark
+    app
+    fixed
+    height="48"
+    class="py-3 elevation-0 dashboard-header"
+  >
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn text to="/">Post</v-btn>
+      <v-btn text to="/gallery">Gallery</v-btn>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+</template>
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  computed: {},
+  methods: {}
+});
+</script>
+<style>
+.dashboard-header.v-toolbar.v-toolbar--fixed {
+  padding-left: 0 !important;
+  position: fixed;
+  z-index: 1;
+}
+</style>
---
+++ src/layout/Layout.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-container fluid pa-0>
+      <Header></Header>
+      <router-view />
+    </v-container>
+  </v-app>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import Header from "./Header.vue";
+
+export default Vue.extend({
+  computed: {},
+  components: {
+    Header
+  },
+  methods: {}
+});
+</script>
